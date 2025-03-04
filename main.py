8ecbacab6c9f387a2752f6c2dee70c58dcc3f4c9
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-03-04 17:08:14 +0545

    shittttttttt

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 from transformers import pipeline
 from transformers.utils.logging import set_verbosity_error
-from langchain_huggingface import HuggingFacePipeline
-from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-
 
 set_verbosity_error()
 
-# Use a pipeline as a high-level helper
-from transformers import pipeline
+model = pipeline("summarization", model="facebook/bart-large-cnn", use_auth_token=False)
 
-model = pipeline("question-answering", model="deepset/roberta-base-squad2")
+text = input("Enter the text to be summarized: ")
 
-template = "generate about {question} in {context}."
-prompt = PromptTemplate.from_template(template)
-parser = StrOutputParser()
+# Generate summary
+response = model(text, max_length=130, min_length=30, do_sample=False)
 
-chain = prompt | model | parser
-
-# Get user input.
-topic = input("Enter the topic you want a description of: ")
-context = input("Enter the context you are talking about.")
-
-# Invoke the chain with a dictionary of prompt variables.
-response = chain.invoke({"question": topic, "context": context})
-
-print("Response:", response)
+# Print the summary
+print("Summary:", response[0]['summary_text'])
