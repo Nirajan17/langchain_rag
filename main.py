80ce3cf1d81a235f4f7fffada610497e69dda280
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-03-04 16:42:24 +0545

    qa

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ from transformers import pipeline
 from transformers.utils.logging import set_verbosity_error
 from langchain_huggingface import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
 
 set_verbosity_error()
 
-pipe = pipeline("text-generation", model="gpt2")
+# Use a pipeline as a high-level helper
+from transformers import pipeline
 
-model = HuggingFacePipeline(pipeline=pipe)
+model = pipeline("question-answering", model="deepset/roberta-base-squad2")
 
-template = "Explain the topic {question} in {context} country in detail."
+template = "generate about {question} in {context}."
 prompt = PromptTemplate.from_template(template)
+parser = StrOutputParser()
 
-chain = prompt | model
+chain = prompt | model | parser
 
 # Get user input.
 topic = input("Enter the topic you want a description of: ")
-context = "Nepal"
+context = input("Enter the context you are talking about.")
 
 # Invoke the chain with a dictionary of prompt variables.
 response = chain.invoke({"question": topic, "context": context})
