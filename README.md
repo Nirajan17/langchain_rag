e1cfd0a3b9238acc326700a7e2102646d684008d
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-03-05 16:25:02 +0545

    working on retreiver

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# langchain
+# Learning langchain
+
+    *how to use huggingface models for inferencing locally
+    *using API calls
+
--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 import getpass
 import os
 from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+import warnings
+
+warnings.filterwarnings("ignore")
 
 load_dotenv()
 
-if not os.getenv("HUGGINGFACEHUB_API_TOKEN"):
+# loading huggingface access token
+HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+if not HUGGINGFACEHUB_API_TOKEN:
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = getpass.getpass("Enter your token: ")
 
+# loading model using huggingfaceendpoint
+repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+
+llm = HuggingFaceEndpoint(
+    repo_id=repo_id,
+    max_length=128,
+    temperature=0.5,
+    huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
+)
+
+# generating the prompt template for the model
+template = """
+question: {question}
+context: {context}
+You are an smart AI assistant and should reply to the {question} given according to the {context} given.
+"""
+
+prompt = PromptTemplate.from_template(template)
+
+chain = prompt | llm
+
+question = input("Enter the question: \n")
+context = input("In which context the question is being asked? \n")
+
+response = chain.invoke({"question": question, "context": context})
+
+print(response)
